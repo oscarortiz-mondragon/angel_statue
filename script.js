9c6311cab23eac512dda7cdb87d5c63008df99be
oscarortiz-mondragon <66816556+oscarortiz-mondragon@users.noreply.github.com>
2021-05-23 19:34:39 -0500

    Added more interactivity with selections
    Worked on adding the ability to check added items, as well as add color of the added items with the use of jquery.

--- script.js
+++ script.js
@@ -17,6 +17,48 @@ $('.outDoor').show();
 
 $('document').ready(function(){
 $(".bAll").on('click',function(){
-$('.col').show();g
+$('.col').show();
 
 })})
+// $('.carousel').carousel({
+//   interval: 2000
+// })
+
+$('document').ready(function(){
+$('i').on('mouseover',function(){
+$('i').css('color','blue')
+})
+
+})
+$('document').ready(function(){
+$('i').on('mouseleave',function(){
+$('i').css('color','black')
+
+})
+
+})
+$('document').ready(function(){
+$('i').on('click',function(){
+$(this).removeClass("fas fa-shopping-cart iCart");
+$(this).addClass("fas fa-check iCheck")
+$(this).text("Added!")
+
+
+})
+})
+$('document').ready(function(){
+  $('i').on('click',function(){
+    $('.iCheck').css('color','green')
+  })
+})
+
+$('document').ready(function(){
+  $('i').on('mouseover',function(){
+    $('.iCheck').css('color','green')
+  })
+})
+
+
+
+
+
